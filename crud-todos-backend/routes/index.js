400dd5590c51a26edd81e9adc716b1b478d22903
odvorko <oleksandrdvorko1703@gmail.com>
2025-03-19 00:12:00 +0000

    app and index js changes

--- crud-todos-backend/routes/index.js
+++ crud-todos-backend/routes/index.js
@@ -1,68 +1,43 @@
 var express = require('express');
 var router = express.Router();
 var sqlite = require("better-sqlite3");
+const db = new sqlite('database.db');
 
-
-/** create
- * @openapi
- * /:
- *   post:
- *     description: 'Task required in the body of the request e.g., {task: "Walk the cat"}'
- *     responses:
- *       201:
- *         description: Returns a blank object.
- */
-router.post('/', function (req, res, next) {
-  var db = new sqlite('database.db');
-  db.prepare('INSERT INTO todos (task) VALUES (?)').run(req.body.task);
-  res.status(201).json({})
+router.get('/todos', function (req, res, next) {
+  try {
+    const todos = db.prepare("SELECT * FROM todos").all();
+    res.json({ todos: todos });
+  } catch (err) {
+    next(err);
+  }
 });
 
-/** read
- * @openapi
- * /:
- *   get:
- *     description: No inputs required
- *     responses:
- *       200:
- *         description: Returns an object of todos.
- */
-router.get('/', function (req, res, next) {
-  var db = new sqlite('database.db');
-  var todos = db.prepare("SELECT * FROM todos").all();
-  res.json({ todos: todos })
+router.post('/todos', function (req, res, next) {
+  try {
+    db.prepare('INSERT INTO todos (task) VALUES (?)').run(req.body.task);
+    res.status(201).json({});
+  } catch (err) {
+    next(err);
+  }
 });
 
-
-/** update
- * @openapi
- * /:
- *   put:
- *     description: "ID and task required in the body of the request e.g., {todo_id: 1, task: "Get food"}"
- *     responses:
- *       204:
- *         description: Returns a blank object.
- */
-router.put('/', function (req, res, next) {
-  var db = new sqlite('database.db');
-  console.log(req.body)
-  db.prepare('UPDATE todos SET task = ? where id = (?)').run(req.body.task, req.body.todo_id);
-  res.status(204).json({})
+router.put('/todos', function (req, res, next) {
+  try {
+    console.log(req.body);
+    db.prepare('UPDATE todos SET task = ? WHERE id = ?').run(req.body.task, req.body.todo_id);
+    res.status(204).json({});
+  } catch (err) {
+    next(err);
+  }
 });
 
-/** delete
- * @openapi
- * /:
- *   delete:
- *     description: "ID data required in the body of the request e.g., {data: {todo_id: 1}}"
- *     responses:
- *       204:
- *         description: Returns a blank object.
- */
-router.delete('/', function (req, res, next) {
-  var db = new sqlite('database.db');
-  db.prepare('DELETE FROM todos where id = (?)').run(req.body.todo_id);
-  res.status(204).json({})
+router.delete('/todos', function (req, res, next) {
+  try {
+    db.prepare('DELETE FROM todos WHERE id = ?').run(req.body.todo_id);
+    res.status(204).json({});
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
