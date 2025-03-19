cded248f1b977b2480e151b6ddd6a97b83d3cfd6
odvorko <oleksandrdvorko1703@gmail.com>
2025-03-19 17:13:54 +0000

    updated app.js

--- crud-todos-backend/app.js
+++ crud-todos-backend/app.js
@@ -14,12 +14,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(cors())
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Front-end 
+app.use(express.static('/home/ubuntu/crud-app/crud-todos'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -31,13 +32,15 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+// Loading server
+app.listen(8080, () => {
+  console.log('Server running on port 8080');
 });
 
 module.exports = app;
