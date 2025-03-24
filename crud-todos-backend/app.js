4713a9a5e793ed300c42f41ed4552f842b46ef4d
odvorko <oleksandrdvorko1703@gmail.com>
2025-03-24 16:10:53 +0000

    upd

--- crud-todos-backend/app.js
+++ crud-todos-backend/app.js
@@ -10,6 +10,7 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -19,20 +20,29 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// API routes with /api prefix
 app.use('/api', indexRouter);
 app.use('/api/users', usersRouter);
 
+// Serve static files (frontend)
 app.use(express.static('/home/ubuntu/crud-app/crud-todos'));
 
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
     next(createError(404));
 });
 
+// error handler
 app.use(function(err, req, res, next) {
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
     res.status(err.status || 500);
     res.render('error');
+});
+
+// Start server
+app.listen(8081, () => {
+    console.log('Server running on port 8081');
 });
 
 module.exports = app;
